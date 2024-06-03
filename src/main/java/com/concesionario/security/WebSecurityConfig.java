e2676f5d955b12e4a5ee5e3fd223f2642ec9916a
mohid <fukahamaroc@gmail.com>
2024-06-03 13:27:45 +0200

    implement spring security

--- src/main/java/com/concesionario/security/WebSecurityConfig.java
+++ src/main/java/com/concesionario/security/WebSecurityConfig.java
@@ -6,11 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 
 @Configuration
@@ -33,11 +36,16 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(toH2Console())
+                        .disable()
+                )
                 .authorizeHttpRequests(authz->
                         authz.requestMatchers(resources).permitAll()
                                 .requestMatchers("/","/register","/login","/error/**","/index","/car-sale","/car-rent","/about","/privacy","/terms","/team","/testimonial","/h2-console/**","/detail","/booking").permitAll()
+
                                 .anyRequest().authenticated())
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .formLogin((form) -> form
                         .loginPage("/login")
                         .defaultSuccessUrl("/")
